b2e1f9c267ca66ac48055b66e54de84d5122618e
vikas satpute <vikas.s.satpute@gmail.com>
2018-12-11 11:29:47 +0530

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ListingComponent } from './listing/listing.component';
+import { DetailComponent } from './detail/detail.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ListingComponent,
+    DetailComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
